9b3819880a14c59211b27ea3485373d602686e28
JakkeYang <jakke.yang@gmail.com>
2016-12-06 13:11:00 +0800

    visualization

--- gini-decisiontree/src/treeutils.py
+++ gini-decisiontree/src/treeutils.py
@@ -7,9 +7,18 @@
 import pydot as pd
 
 def genTreeGraph(nodeList):
+    graph = pd.Dot(graph_type='graph')
     for node in nodeList:
         if not node.parent is None:
-            print (node.name+node.parent.name)
+            # print (node.name+node.parent.name)
+#             tmpNode = pd.Node(name=node.name, nodehash=hash(node))
+#             tmpParentNode = pd.Node(name=node.parent.name, nodehash=hash(node.parent))
+            tmpNode = pd.Node(name=hash(node))
+            tmpParentNode = pd.Node(name=hash(node.parent))
+            tmpEdge = pd.Edge(tmpParentNode,tmpNode, label=3)
+            graph.add_edge(tmpEdge)
+#             graph.add_node(tmpNode)
+    graph.write_jpeg('decisiontree.jpg', prog='dot')
     return True
 
 # graph = pd.Dot(graph_type='graph')
